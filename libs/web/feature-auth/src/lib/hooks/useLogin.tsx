8b28289c2abe96119017ee2a040d7af5c56202a7
Leah Anestos <leahkanestos@gmail.com>
2023-12-06 20:33:50 -0500

    Noted addressed form previous commit

--- libs/web/feature-auth/src/lib/hooks/useLogin.tsx
+++ libs/web/feature-auth/src/lib/hooks/useLogin.tsx
@@ -1,86 +1,81 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useRouter } from 'next/router';
 import { validateEmail } from '@daruma-board/web/design-system';
 
-
-
 export const useLogin = () => {
-    const [email, setEmail] = useState<string>('');
-    const [auth, setAuth] = useState<boolean>(false); // [auth, setAuth
-    const [loading, setLoading] = useState<boolean>(false); // [loading, setLoading
-    const router = useRouter();
+  const [email, setEmail] = useState<string>('');
+  const [auth, setAuth] = useState<boolean>(false); // [auth, setAuth
+  const [loading, setLoading] = useState<boolean>(false); // [loading, setLoading
+  const [code, setCode] = useState('');
+  const router = useRouter();
 
+  const handleLogin = async (email: string) => {
+    setLoading(true);
 
-    const handleLogin = async (email: string) => {
-      setLoading(true);
-
-      if (!email) {
-        alert('Email is required');
-        setLoading(false);
-        return;
-      }
-  
-      if (!validateEmail(email)) {
-        alert('Email is invalid');
-        setLoading(false);
-        return;
-      }
-  
-      await fetch('/api/auth-email', {
-        method: 'POST',
-        body: JSON.stringify({ email }),
-      })
-        .then((res) => {
-          if (res.status === 200) {
-            console.log(res);
-            setAuth(true);
-          } else {
-            if (res.status === 403) {
-              alert('Request for dev mode denied, use testing address');
-            } else {
-              alert('Email not sent: ' + res.status + ' ' + res.statusText);
-            }
-          }
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-        });
+    if (!email) {
+      alert('Email is required');
       setLoading(false);
-    };
-    
-    const [code, setCode] = useState('');
-    const validateToken = async (code: 'string', email: string) => {
-      setLoading(true);
-      await fetch('/api/auth-token', {
-        method: 'POST',
-        body: JSON.stringify({ token: code, email }),
-      })
-        .then(async (res) => {
-          const body = await res.json();
-          if (res.status === 200) {
-            localStorage.setItem('token', body.token);
-            localStorage.setItem('localEmail', email);
-  
-            router.push('/');
-          } else {
-            alert('Invalid code');
-          }
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-        });
+      return;
+    }
+
+    if (!validateEmail(email)) {
+      alert('Email is invalid');
       setLoading(false);
-    }; 
+      return;
+    }
 
-    return {
-      email,
-      setEmail,
-      auth,
-      loading,
-      handleLogin,
-      code,
-      setCode,
-      validateToken,
-    };   
+    await fetch('/api/auth-email', {
+      method: 'POST',
+      body: JSON.stringify({ email }),
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          setAuth(true);
+        } else {
+          if (res.status === 403) {
+            alert('Request for dev mode denied, use testing address');
+          } else {
+            alert('Email not sent: ' + res.status + ' ' + res.statusText);
+          }
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+    setLoading(false);
+  };
 
+  const validateToken = async (code: string, email: string) => {
+    setLoading(true);
+    await fetch('/api/auth-token', {
+      method: 'POST',
+      body: JSON.stringify({ token: code, email }),
+    })
+      .then(async (res) => {
+        const body = await res.json();
+        if (res.status === 200) {
+          localStorage.setItem('token', body.token);
+          localStorage.setItem('localEmail', email);
+
+          router.push('/');
+        } else {
+          alert('Invalid code');
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+    setLoading(false);
+  };
+
+  return {
+    email,
+    setEmail,
+    auth,
+    loading,
+    handleLogin,
+    code,
+    setCode,
+    validateToken,
+  };
 };
--- libs/web/feature-auth/src/lib/pages/LoginPage.tsx
+++ libs/web/feature-auth/src/lib/pages/LoginPage.tsx
@@ -1,76 +1,12 @@
-import { PageLayout, validateEmail } from '@daruma-board/web/design-system';
-import { useState } from 'react';
-import { useRouter } from 'next/router';
+import { PageLayout} from '@daruma-board/web/design-system';
 import { LoginBox } from '../components';
+import { useLogin } from '../hooks/useLogin';
 
 
 export const LoginPage = () => {
-  // TODO: move to hook
-  const [email, setEmail] = useState<string>('');
-  const [auth, setAuth] = useState<boolean>(false); // [auth, setAuth
-  const [loading, setLoading] = useState<boolean>(false); // [loading, setLoading
-  const router = useRouter();
-  const handleLogin = async (email: string) => {
-    setLoading(true);
+  const { email, setEmail, auth, code, setCode, handleLogin, validateToken, loading } = useLogin();
 
-    // Validate
-    if (!email) {
-      alert('Email is required');
-      setLoading(false);
-      return;
-    }
 
-    if (!validateEmail(email)) {
-      alert('Email is invalid');
-      setLoading(false);
-      return;
-    }
-
-    await fetch('/api/auth-email', {
-      method: 'POST',
-      body: JSON.stringify({ email }),
-    })
-      .then((res) => {
-        if (res.status === 200) {
-          console.log(res);
-          setAuth(true);
-        } else {
-          if (res.status === 403) {
-            alert('Request for dev mode denied, use testing address');
-          } else {
-            alert('Email not sent: ' + res.status + ' ' + res.statusText);
-          }
-        }
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-    setLoading(false);
-  };
-
-  const [code, setCode] = useState<string>('');
-  const validateToken = async (code: string, email: string) => {
-    setLoading(true);
-    await fetch('/api/auth-token', {
-      method: 'POST',
-      body: JSON.stringify({ token: code, email }),
-    })
-      .then(async (res) => {
-        const body = await res.json();
-        if (res.status === 200) {
-          localStorage.setItem('token', body.token);
-          localStorage.setItem('localEmail', email);
-
-          router.push('/');
-        } else {
-          alert('Invalid code');
-        }
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-    setLoading(false);
-  };
 
   return (
     <PageLayout>
