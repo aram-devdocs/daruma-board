3992f2c874df18015e23b81a05441703b57477ef
Leah Anestos <leahkanestos@gmail.com>
2023-12-14 20:30:59 -0500

    reverted back to access of material UI

--- libs/web/design-system/src/lib/styles/theme.tsx
+++ libs/web/design-system/src/lib/styles/theme.tsx
@@ -2,11 +2,45 @@ import { ThemeOptions } from '@mui/material/styles';
 
 
 const colorPalette = {
-  color: {
-    white: '#ffffff',
-  }
-
+  primary: {
+    main: '#3f51b5',
+    light: '#757de8',
+    dark: '#002984',
+    contrastText: '#fff',
+  },
+  secondary: {
+    main: '#f50057',
+    light: '#ff4081',
+    dark: '#c51162',
+    contrastText: '#fff',
+  },
+  error: {
+    main: '#f44336',
+    light: '#e57373',
+    dark: '#d32f2f',
+    contrastText: '#fff',
+  },
+  warning: {
+    main: '#ff9800',
+    light: '#ffb74d',
+    dark: '#f57c00',
+    contrastText: '#fff',
+  },
+  info: {
+    main: '#2196f3',
+    light: '#64b5f6',
+    dark: '#1976d2',
+    contrastText: '#fff',
+  },
+  success: {
+    main: '#4caf50',
+    light: '#81c784',
+    dark: '#388e3c',
+    contrastText: '#fff',
+  },
 };
+
+
 
 export const lightThemeOptions: ThemeOptions = {
   palette: {
